674a41ae5987f4ebb8840e4f7499cd0a234a2f53
dzcoder21 <siddhantkumar06448@gmail.com>
2025-10-27 13:49:19 +0530

    new files Added

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const path = require("path");
 const connectDB = require("./config/db.js");
 const authRoutes = require("./routes/authRoutes");
 const incomeRoutes = require("./routes/incomeRoutes");
+const expenseRoutes = require("./routes/expenseRoutes");
 const app = express();
 
 app.use(
@@ -21,6 +22,7 @@ connectDB();
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/income", incomeRoutes);
+app.use("/api/v1/expense", expenseRoutes);
 
 //serve uploads folder
 
