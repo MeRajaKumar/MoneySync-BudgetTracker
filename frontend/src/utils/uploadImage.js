60dfecd997f19f7b4bc6696e149ec1fb28ca1c17
Raja Kumar <teamaayushgupta@gmail.com>
2025-10-29 21:54:39 +0530

    all changes here

--- frontend/src/utils/uploadImage.js
+++ frontend/src/utils/uploadImage.js
@@ -1,3 +1,4 @@
+// chatGpt
 import { API_PATHS } from "./apiPaths";
 import axiosInstance from "./axiosInstance"; // <- MUST be relative
 
@@ -28,9 +29,9 @@ export default uploadImage;
 
 
 
-
 // import { API_PATHS } from "./apiPaths";
-// import axiosInstance from "axiosInstance";
+// import axiosInstance from "./axiosInstance";
+
 
 // const uploadImage = async (imageFile) => {
 //     const formData =  new FormData();
@@ -40,7 +41,7 @@ export default uploadImage;
 //     try{
 //         const response = await axiosInstance.post(API_PATHS.IMAGE.UPLOAD_IMAGE, formData, {
 //             headers: {
-//                 'Content-Type': 'multipart/form-data',// set header for file uplaod
+//                 'Content-Type': 'multipart/form-data', // set header for file uplaod
 //             },
 
 //         });
