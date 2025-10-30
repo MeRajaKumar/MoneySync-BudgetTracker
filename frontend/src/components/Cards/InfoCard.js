49cf7cd411743feef3c3f02bced9323c719d58e7
dzcoder21 <siddhantkumar06448@gmail.com>
2025-10-30 12:18:23 +0530

    all commits

--- frontend/src/components/Cards/InfoCard.js
+++ frontend/src/components/Cards/InfoCard.js
@@ -1,16 +1,21 @@
 import React from "react";
+import { IoMdCard } from "react-icons/io";
 
-const InfoCard = (
-    <InfoCard
-                icon={<IoMdCard />}
-                label="Total Balance"
-                value={addThousandsSeparator(dashboardData?.totalBalance || 0)}
-                color="bg-primary"
-                />
-) =>{
+
+
+const InfoCard = ({ icon, label, value, color }) => {
+ 
     return (
-        <div>InfoCard</div>
+        <div className="flex gap-6 bg-white p-6 rounded-2xl shadow-md shadow-gray-100 border border-gray-500/5  ">
+            <div className={`w-14 h-14 flex item-center  justify-center text-[26px] text-white ${color} rounded-full drop-shadow-xl`}>
+                {icon}
+            </div>
+            <div>
+                <h6 className="text-sm text-gray-500 mb-1">{label}</h6>
+                <span className="text-[22px]">${value}</span>
+            </div>InfoCard
+        </div>
     )
-}
+};
 
 export default InfoCard;
