d11ca121aa2f56d0be8035960eb24776f0d60480
Raja Kumar <teamaayushgupta@gmail.com>
2025-10-30 12:19:33 +0530

    all commit till now

---
+++ frontend/src/pages/Dashboard/Home.jsx
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from 'react'
+import DashboardLayout from '../../components/layouts/DashboardLayout';
+import { useUserAuth } from '../../hooks/useUserAuth';
+import { useNavigate } from 'react-router-dom';
+import axiosInstance from '../../utils/axiosInstance';
+import { API_PATHS } from '../../utils/apiPaths'; 
+import InfoCard from "../../components/Cards/InfoCard.js";
+
+
+const Home = () => {
+  useUserAuth();
+
+  const navigate = useNavigate();
+
+  const [dashboardData, setDashboardData ] = useState(null);
+  const [loading,setLoading] = useState(false);
+
+  const fetchDashboardData = async () => {
+    setLoading(true);
+
+    try{
+      const response = await axiosInstance.get(
+        `${API_PATHS.DASHBOARD.GET_DATA}`
+      );
+
+      if ( response.data){
+        setDashboardData(response.data);
+      }
+
+    } catch ( error ){
+      console.log("Something went wrong. Please try again.",error);
+    }finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchDashboardData();
+    return () => {};
+  },[]);
+
+  return (
+    <DashboardLayout activeMenu = "Dashboard">
+      <div className="my-5 mx-auto">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+          <InfoCard
+            icon={<IoMdCard />}
+            label="Total Balance"
+            value={addThousandsSeparator(dashboardData?.totalBalance || 0)}
+            color="bg-primary"
+            />
+
+
+            <FinanceOverview
+              totalBalance={dashboarData?.totalBalance || 0}
+              totalIncome={dashboardData?.totalIncome || 0}
+              toalExpense={dashboardData?.tatalExpenses || 0}
+              />
+            </div>
+      </div>
+    </DashboardLayout>
+  );
+};
+
+export default Home;
